e38e0577f80f1361e88eccb332ac5239c7f56d8e
ajh123 <ajh123@users.noreply.github.com>
2022-04-16 13:21:05 +0100

    Begin installer again

---
+++ installer.lua
@@ -0,0 +1,15 @@
+local args = dofile("argser.lua")(...)
+  :named("remove", false, "boolean"):alias("r"):default(false)
+  :named("setup", false, "boolean"):alias("s"):default(false)
+  :parse().args
+
+for i, arg in ipairs(args) do
+  print(i .. ". " .. arg)
+end
+
+if args.remove then
+  fs.delete("/gtmos")
+end
+if args.setup then
+  print("No setup, for now...")
+end
