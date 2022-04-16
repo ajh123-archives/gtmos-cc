1e6fb732dbfae39c2bb358c56135dbf71c23ab54
ajh123 <ajh123@users.noreply.github.com>
2022-04-16 14:42:20 +0100

    installer 5 fix

--- installer.lua
+++ installer.lua
@@ -3,10 +3,10 @@ local args = dofile("/gtmos/argser.lua")(...)
   :named("setup", true, "boolean"):alias("s"):default(false)
   :parse().args
 
-if args["remove"] then
+if args.remove then
   fs.delete("/gtmos")
 end
-if args["setup"] then
+if args.setup then
   fs.makeDir("/Users")
   print("No setup, for now...")
 end
