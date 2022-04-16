8e78fc8489d01c5a6833ef04dff6e65fecddfa8f
ajh123 <ajh123@users.noreply.github.com>
2022-04-16 14:37:56 +0100

    installer 4

--- installer.lua
+++ installer.lua
@@ -1,6 +1,6 @@
 local args = dofile("/gtmos/argser.lua")(...)
-  :named("remove", false, "boolean"):alias("r"):default(false)
-  :named("setup", false, "boolean"):alias("s"):default(false)
+  :named("remove", true, "boolean"):alias("r"):default(false)
+  :named("setup", true, "boolean"):alias("s"):default(false)
   :parse().args
 
 for key, value in pairs(args) do
