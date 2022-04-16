10cd97481845e4e73e15307f1a727d902b4d42f2
ajh123 <ajh123@users.noreply.github.com>
2022-04-16 14:38:45 +0100

    installer 5

--- installer.lua
+++ installer.lua
@@ -3,12 +3,6 @@ local args = dofile("/gtmos/argser.lua")(...)
   :named("setup", true, "boolean"):alias("s"):default(false)
   :parse().args
 
-for key, value in pairs(args) do
-  print(key, value)
-end
-
-print(args)
-
 if args["remove"] then
   fs.delete("/gtmos")
 end
